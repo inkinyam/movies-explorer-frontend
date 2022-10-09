63158d19e45178794f9205008f1b7ce4b85030e9
inki <inkinyam@yandex.ru>
2022-10-09 18:48:02 +0300

    feat: обработка куррент юзер в профайл, апдейт данных, валидация формы в профайл

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -5,99 +5,113 @@ import { useInputValidator } from '../../utils/customHooks/inputValidator';
 
 const Profile = ({onUpdateUser ,onSignOut}) => {
   const currentUser = React.useContext(CurrentUserContext);
-  const [isEditInput, setIsEditInput] = React.useState(true);
-/*   const controlInput = useInputValidator();
-  const { nameErr, emailErr } = controlInput.errors; 
+  const [isUnlock, setIsUnlock] = React.useState(true);
 
-  const errorClassName = !controlInput.isValid
+  const inputControl = useInputValidator();
+  const {name, email} = inputControl.errors;
+  const [isSameValues, setIsSameValues] = React.useState(true);
+  const [isValid, setIsValid] = React.useState(false);
+  const [isSucces, setIsSucces] = React.useState(false);
+
+  const toggleStateInput = (e) => {
+    e.preventDefault();
+    setIsUnlock(!isUnlock);
+    setIsSucces(false);
+  }; 
+
+  const button = isUnlock 
+    ? <button type="button"  
+              className="profile__button profile__button_v_edit" 
+              onClick={toggleStateInput}> Редактириовать </button>
+    : <button type="submit" 
+              className={`profile__button profile__button_v_edit ${(!isValid && 'profile__button_disable')}`}
+              disabled={(!isValid && 'disabled')}> Сохранить </button>
+  
+  const errorClassName = !isValid
     ? 'profile__error profile__error_show'
-    : 'profile__error';*/
+    : 'profile__error';
 
-  const resultClassName = !isEditInput 
+
+  const succesClassName = isSucces 
     ? 'profile__succes profile__succes_show'
     : 'profile__succes';
 
-  const toggleStateInput = (e) => {
-     e.preventDefault();
-    setIsEditInput(!isEditInput);
-  };
 
-const [nameValue, setNameValue] = React.useState();
-const [emailValue, setEmailValue] = React.useState();
+  React.useEffect(() => {
+    if (inputControl.values.name === currentUser.name &&
+        inputControl.values.email === currentUser.email) {
+          setIsSameValues(false);
+    } else {
+      setIsSameValues(true);
+    }
+  }, [currentUser, inputControl.values.name, inputControl.values.email]);
 
-const handleEmailChange = (e) => {
-  setEmailValue(e.target.value);
-}
+  React.useEffect(() => {
+    setIsValid(inputControl.isValid && isSameValues)
+  }, [isSameValues, inputControl.isValid]);
 
-const handleNameChange = (e) => {
-  setNameValue(e.target.value);
-}
 
   /*обработчик сабмита формы*/
-  const handleSubmit = (e) => {
+  function handleSubmit(e)  {
     e.preventDefault();
-
-    const { name, email } = {nameValue, emailValue};
-    if (!name) {
-      onUpdateUser(currentUser.name, email);
-    } else if (!email) {
-      onUpdateUser(name, currentUser.email);
-    } else {
-      onUpdateUser(name, email);
+    let { name, email } = inputControl.values;
+      if (!name) {
+        name = currentUser.name;
+      } else if (!email) {
+        email = currentUser.email
+      } 
+      onUpdateUser({name, email});
+      setIsSucces(true);
+      setTimeout(() => setIsUnlock(!isUnlock), 1000);
+      setTimeout(() => setIsSucces(false), 1000);
+      inputControl.resetForm();
     }
-    setTimeout(() => setIsEditInput((state) => !state), 1000);
-  };
 
 
   return (
     <>
     <Navigation />
-    <section className="profile" onSubmit={handleSubmit}>
+    <section className="profile" >
       <h2 className="profile__title">Привет, {currentUser.name}</h2>
 
-      <form className="profile__form" noValidate>
+      <form className="profile__form" noValidate onSubmit={handleSubmit}>
         <div className="profile__form-block">
-          <input className="profile__input" 
+          <input className={`profile__input ${inputControl?.errors?.name && "profile__input_error"}`} 
                  type="text" 
+                 name="name"
                  placeholder={currentUser.name}
                  id="profileName" 
                  minLength="2" 
                  maxLength="40" 
                  required 
-                 pattern='[A-Za-zА-Яа-яЁё\s-]+'
-                 onChange={handleNameChange}
-                 value={nameValue ?? currentUser.name}
-                 {...(!isEditInput ? {} : { disabled: true })}/>
+                 pattern="[A-Za-zА-Яа-яЁё\s-]+"
+                 value={inputControl?.values?.name ?? currentUser.name}
+                 onChange={inputControl.handleChange}
+                 disabled={isUnlock && "disabled"}
+                />
           <label className="profile__label" htmlFor="profileName">Имя</label>
         </div>
         <div className="profile__form-block">
-          <input className="profile__input" 
+          <input className={`profile__input ${inputControl?.errors?.email && "profile__input_error"}`}
                  type="email" 
+                 name="email" 
                  id="profileEmail" 
-                 minLength="2" 
-                 maxLength="40" 
                  required
                  placeholder={currentUser.email}
-                 pattern='^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
-                 onChange={handleEmailChange}
-                 value={emailValue ?? currentUser.email}
-                 {...(!isEditInput ? {} : { disabled: true })}
+                 pattern="^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$"
+                 value={inputControl?.values?.email ?? currentUser.email}
+                 onChange={inputControl.handleChange}
+                 disabled={isUnlock && "disabled"}
                  />
           <label className="profile__label"  htmlFor="profileEmail">E-mail</label>
         </div>
-{/*         <span className={errorClassName}>{emailErr}</span>
-        <span className={errorClassName}>{nameErr}</span> */}
-        <span className={resultClassName}>Изменения прошли успешно!</span>
+
+        <span className={errorClassName}>{name}{email}</span> 
+        <span className={succesClassName}>Изменения произведены успешно!</span> 
+
+        {button}
+        <button type="button" className="profile__button profile__button_v_exit" onClick={onSignOut}>Выйти из аккаунта</button> 
       </form>
-
-      
-   <button type="button" 
-              className="profile__button profile__button_v_edit"   
-              onClick={toggleStateInput}>
-              Редактировать
-      </button>
-      <button type="button" className="profile__button profile__button_v_exit" onClick={onSignOut}>Выйти из аккаунта</button> 
-
     </section>
     </>
   )
